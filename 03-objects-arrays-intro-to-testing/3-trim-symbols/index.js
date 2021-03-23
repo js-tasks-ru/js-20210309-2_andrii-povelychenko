811a0f61ca58a0d7a4f7ee42ae6a241204a57af0
Andrii Povelychenko <anryogo@gmail.com>
2021-03-23 11:03:17 +0200

    03.3 trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,19 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === 0) {
+    return '';
+  }
 
+  if (!size) {
+    return string;
+  }
+
+  return string.split('').reduce((result, symbol) => {
+    if (result.slice(-size) !== symbol.repeat(size)) {
+      result += symbol;
+    }
+
+    return result;
+  }, '');
 }
